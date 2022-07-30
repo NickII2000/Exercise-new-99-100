ae0a42ea6b55eafb487f9f82bde8d71b41209f39
Nick <Nick_II2000@mail.ru>
2022-07-30 20:35:32 +0600

    8-th commit

--- script.js
+++ script.js
@@ -50,34 +50,58 @@ const ivan = new User('Ivan', 27);
 
 
 
-function User(name, age) {
-    this.name = name;
-    let userAge = age;
+// function User(name, age) {
+//     this.name = name;
+//     let userAge = age;
 
-    this.say = function () {
-        console.log(`Имя пользователя: ${this.name}, возраст: ${userAge}`);
-    };
+//     this.say = function () {
+//         console.log(`Имя пользователя: ${this.name}, возраст: ${userAge}`);
+//     };
 
-    this.getAge = function () {
-        return userAge;
-    };
+//     this.getAge = function () {
+//         return userAge;
+//     };
 
-    this.setAge = function (age) {
+//     this.setAge = function (age) {
+//         if (typeof age === 'number' && age > 0 && age < 110) {
+//             userAge = age;
+//         } else {
+//             console.log('Недопустимое значение!');
+//         }
+//     };
+
+// }
+
+// const ivan = new User('Ivan', 27);
+// console.log(ivan.name);
+// console.log(ivan.getAge());
+// ivan.say();
+
+// ivan.setAge(52);
+// ivan.setAge(300);
+// ivan.name = 'Nick';
+// ivan.say();
+
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        let userAge = age;
+    }
+
+    say() {
+        console.log(`Имя пользователя: ${this.name}, возраст: ${this._age}`);
+    },
+
+    getAge() {
+        return UserAge;
+    }
+
+    setAge(age) {
         if (typeof age === 'number' && age > 0 && age < 110) {
-            userAge = age;
+            this._age = age;
         } else {
             console.log('Недопустимое значение!');
         }
-    };
-
+    }
 }
-
-const ivan = new User('Ivan', 27);
-console.log(ivan.name);
-console.log(ivan.getAge());
-ivan.say();
-
-ivan.setAge(52);
-ivan.setAge(300);
-ivan.name = 'Nick';
-ivan.say();
