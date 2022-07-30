09576b779da81f4a500d343acc012b4d7adc87c3
Nick <Nick_II2000@mail.ru>
2022-07-30 20:15:02 +0600

    2-th commit

--- script.js
+++ script.js
@@ -47,3 +47,24 @@ class User {
 }
 
 const ivan = new User('Ivan', 27);
+
+
+
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+
+    this.say = function () {
+        console.log(`Имя пользователя: ${this.name}, возраст ${this.age}`);
+    }
+
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.name);
+console.log(ivan.age);
+ivan.say();
+
+ivan.age = 52;
+ivan.name = 'Nick';
+ivan.say();
